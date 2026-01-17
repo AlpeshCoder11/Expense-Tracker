cb664c4f2fc72d20c10d4e300cfe37f5ad6fca02
AlpeshCoder11 <aalpeshahir88@gmail.com>
2026-01-17 14:25:42 +0530

    add dark light mode

--- navbar.js
+++ navbar.js
@@ -1,6 +1,17 @@
 const hamburger = document.querySelector("#hamburger");
 const mobileNav = document.querySelector("#mobileNav");
+const modebtn = document.querySelector(".modebtn");
+const main =document.querySelector("body");
+const navSec =document.querySelector(".nav-section")
+const bText =document.querySelectorAll("p");
 
 hamburger.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
 });
+modebtn.addEventListener("click",()=>{
+   main.classList.toggle("modeDL");
+   navSec.classList.toggle("modeDL");
+   bText.forEach(element => {
+    element.classList.toggle("dmtext");
+   });
+})
