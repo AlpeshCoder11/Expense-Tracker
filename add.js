3fbe0cc9d474bbb82960ccc6c16400d3b311e7fa
AlpeshCoder11 <aalpeshahir88@gmail.com>
2026-01-19 17:36:03 +0530

    add working expense button and show remaing

--- add.js
+++ add.js
@@ -4,37 +4,68 @@ const welcomeText = document.querySelector(".welcomeText");
 const ipopup = document.querySelector(".overlay");
 const closeBtn=document.querySelector(".closebtn");
 const popsubtn = document.querySelector(".popsubtn");
+const popsubtn2 = document.querySelector(".popsubtn2");
 const iMoney = document.querySelector(".imoney");
+const eMoney =document.querySelector(".emoney");
+const rMoney =document.querySelector(".rmoney");
+const incomePop =document.querySelector(".textp");
+const expensePop =document.querySelector(".textp2");
 let incomeVal=document.querySelector("#incomeval");
-
+let expenseVal=document.querySelector("#expenseval");
 let totalIncome=0;
+let totalExpense=0;
+let totalRemaining=0;
 let userName = "alpesh"
  
 
 welcomeText.innerText="Welecome, "+userName;
 
-let expenseVal;
+
 newIncome.addEventListener("click",()=>{
   ipopup.classList.add("overlay1");
+  expensePop.classList.add("overlay");
+  incomePop.classList.remove("overlay");
+
 });
 newExpense.addEventListener("click",()=>{
-    expenseVal= prompt("enter expense");
+    ipopup.classList.add("overlay1");
+    incomePop.classList.add("overlay");
+    expensePop.classList.remove("overlay");
 });
 closeBtn.addEventListener("click",()=>{
   ipopup.classList.remove("overlay1");
 });
 popsubtn.addEventListener("click", () => {
- 
+
  let enteredIncome = Number(incomeVal.value);
-
-
+ if(enteredIncome<0){
+  alert("enert valid value");
+ }
+else{
 
    totalIncome += enteredIncome;
+   totalRemaining=totalIncome-totalExpense;
 
   iMoney.innerText = "\u20B9"+totalIncome;
   incomeVal.value = "";
+  rMoney.innerText="\u20B9"+totalRemaining;
+  ipopup.classList.remove("overlay1");}
+});
+popsubtn2.addEventListener("click", () => {
+ 
+ let enteredIncome = Number(expenseVal.value);
+if(enteredIncome<0){
+  alert("enert valid value");
+ }
+else{
 
-  ipopup.classList.remove("overlay1");
+
+  totalExpense += enteredIncome;
+  totalRemaining=totalIncome-totalExpense;
+  eMoney.innerText = "\u20B9"+totalExpense;
+  expenseVal.value = "";
+  rMoney.innerText="\u20B9"+totalRemaining;
+  ipopup.classList.remove("overlay1");}
 });
 
 
