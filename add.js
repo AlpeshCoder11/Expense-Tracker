cafbe96053275892a056381aa8176b131f3a4c07
AlpeshCoder11 <aalpeshahir88@gmail.com>
2026-01-25 10:04:49 +0530

    deploy error fix

--- add.js
+++ add.js
@@ -2,7 +2,9 @@ const newIncome = document.querySelector(".newincome");
 const newExpense = document.querySelector(".newexpense");
 const ipopup = document.querySelector(".overlay");
 const incomePop =document.querySelector(".textp");
-const expensePop =document.querySelector(".textp2");
+const expensePop =document.querySelector(".textp2")
+
+
 
 const closeBtn=document.querySelector(".closebtn");
 
@@ -38,4 +40,3 @@ newExpense.addEventListener("click",()=>{
 closeBtn.addEventListener("click",()=>{
   ipopup.classList.remove("overlay1");
 });
-
--- history.js
+++ history.js
@@ -2,7 +2,7 @@ const newIncome = document.querySelector(".newincome");
 const newExpense = document.querySelector(".newexpense");
 const ipopup = document.querySelector(".overlay");
 const incomePop =document.querySelector(".textp");
-const expensePop =document.querySelector(".textp2");
+const expensePop =document.querySelector(".textp2")
 
 const closeBtn=document.querySelector(".closebtn");
 
@@ -42,5 +42,4 @@ newExpense.addEventListener("click",()=>{
 });
 closeBtn.addEventListener("click",()=>{
   ipopup.classList.remove("overlay1");
-
 });
