fed74e3699b477a0032c78ff9a1e5ae2d4ea483f
AlpeshCoder11 <aalpeshahir88@gmail.com>
2026-02-02 22:44:16 +0530

    improve dark mode ui

--- navbar.js
+++ navbar.js
@@ -1,17 +1,37 @@
 const hamburger = document.querySelector("#hamburger");
 const mobileNav = document.querySelector("#mobileNav");
 const modebtn = document.querySelector(".modebtn");
+const dateDM =document.querySelector(".date-range-container")
 const main =document.querySelector("body");
 const navSec =document.querySelector(".nav-section")
+
 const bText =document.querySelectorAll("p");
 
 hamburger.addEventListener("click", () => {
     mobileNav.classList.toggle("active");
 });
-modebtn.addEventListener("click",()=>{
-   main.classList.toggle("modeDL");
-   navSec.classList.toggle("modeDL");
-   bText.forEach(element => {
-    element.classList.toggle("dmtext");
-   });
-})
+
+  modebtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("modeDL");
+
+    // Save preference
+    if(document.body.classList.contains("modeDL")){
+        localStorage.setItem("theme","dark");
+        modebtn.innerText = "Light";
+    }else{
+        localStorage.setItem("theme","light");
+        modebtn.innerText = "Dark";
+    }
+});
+
+// Load saved theme
+window.addEventListener("load", () => {
+    const theme = localStorage.getItem("theme");
+
+    if(theme === "dark"){
+        document.body.classList.add("modeDL");
+        modebtn.innerText = "Light";
+    }
+});
+
